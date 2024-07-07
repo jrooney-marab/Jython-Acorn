ff9983cc7b182a0bc46dd953d5079247a2b09770
jrooney-marab <rooney-a-mara90@hotmail.com>
2024-07-07 06:41:02 -0700

    Create bashrc.sh

---
+++ bashrc.sh
@@ -0,0 +1,24 @@
+#!shebang/bash/bin/bash --Src
+
+b = $0;
+v = $1;
+v == $v;
+$a = 20;
+a = r;
+r = $a;
+
+  for ( b=0; b < 1; b++) {
+    ++$v;
+  $echo $v + "There's a dns-rouker snake in my boot -! " + b;
+}
+
+  for ( v == 1; v > 0; ++v) {
+    ++$b;
+    $echo $b + "There's a dns-rouker snake in my boot -! " + $a;
+
+} while ( b > 0 ) {
+    ++a;
+    $echo r + "There's a dns-rouker Snake in m boot !~ " + $a;
+} elif (r > b) {
+    r++;
+}
